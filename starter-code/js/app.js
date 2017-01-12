e2443f99c7252d3e2c5a5eaaf1ecc7e7c787ef5a
johnson-rl <johnson.rl@gmail.com>
2017-01-11 22:54:35 -0800

    random word, restart, and score added

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -5,14 +5,32 @@ angular
   .module("hangmanApp", [])
   .controller("HangmanController", HangmanController);
 
+var wordArray = [
+  'elephant',
+  'giraffe',
+  'ninjakitty',
+  'monkey',
+  'zebra',
+  'nakedmolerat'
+];
+
   function HangmanController () {
     var vm = this;
-    vm.guessedLetter = ''
-    vm.game = new HangmanGame('elephant');
-    console.log(vm.game)
+    vm.guessedLetter = '';
+    vm.win = 0;
+    vm.loss = 0;
+    vm.startGame = function (){
+      var wordChoice = Math.round(Math.random()*5);
+      vm.game = new HangmanGame(wordArray[wordChoice]);
+    };
     vm.guessTheLetter = function (){
-      vm.game.guess(vm.guessedLetter)
-      vm.guessedLetter = ''
+      vm.game.guess(vm.guessedLetter);
+      vm.guessedLetter = '';
+      if(vm.game.gameWon===true){vm.win++};
+      if(vm.game.gameWon===false){
+        vm.loss++;
+        vm.game.gameWon = null;
+      };
     }
-    vm.one=true
+    vm.startGame();
   }
