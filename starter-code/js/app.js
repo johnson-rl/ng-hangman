c00340b5061d31aae35d72a24fa10b8be1447d8f
johnson-rl <johnson.rl@gmail.com>
2017-01-11 22:11:01 -0800

    regular challenges complete and hangman draws

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -2,4 +2,17 @@ console.log('app.js loaded!');
 
 // initialize the application
 angular
-  .module("hangmanApp", []);
+  .module("hangmanApp", [])
+  .controller("HangmanController", HangmanController);
+
+  function HangmanController () {
+    var vm = this;
+    vm.guessedLetter = ''
+    vm.game = new HangmanGame('elephant');
+    console.log(vm.game)
+    vm.guessTheLetter = function (){
+      vm.game.guess(vm.guessedLetter)
+      vm.guessedLetter = ''
+    }
+    vm.one=true
+  }
